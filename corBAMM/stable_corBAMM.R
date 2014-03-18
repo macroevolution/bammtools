ac9dcbff33a895fb83103e0bd7c07df9d2534cca
blueraleigh <mcgrundler@gmail.com>
2014-03-18 13:08:09 -0400

    Estimate psi and lambda in corBAMM

--- corBAMM/stable_corBAMM.R
+++ corBAMM/stable_corBAMM.R
@@ -1,97 +1,29 @@
 ## A file to put relatively stable working versions for corBAMM stuff
 
 ##########
-# Implements corBAMM error structure
-##########
-corBAMM <- function(value = 1, ephy, form = ~1) 
-{
-	if (class(ephy) != "bammdata")
-	    stop("arg 'ephy' is not of class 'bammdata'");
-	attr(value, "formula") <- form;
-	attr(value, "fixed") <- TRUE;
-	attr(value, "bammdata") <- ephy;
-	class(value) <- c("corBAMM", "corStruct");
-	return(value);
-}
-
-Initialize.corBAMM <- function(object, data, ...)
-{
-    form <- formula(object);
-    if (!is.null(getGroupsFormula(form))) {
-        attr(object, "groups") <- getGroups(object, form, data = data);
-        attr(object, "Dim") <- Dim(object, attr(object, "groups"));
-    } 
-    else { 
-        attr(object, "Dim") <- Dim(object, as.factor(rep(1, nrow(data))));
-    }
-    attr(object, "covariate") <- getCovariate(object, data = data);
-    attr(object, "Eb") <- getCohortMatrix(attr(object, "bammdata"));
-    
-    phy <- as.phylo.bammdata(attr(object, "bammdata"));
-    if (is.null(data)) data <- parent.frame();
-    if (nrow(data) != length(phy$tip.label))
-        stop("number of observations and number of tips in the tree are not equal.");
-    if (is.null(rownames(data))) {
-        warning("No rownames supplied in data frame, data taken to be in the same order as in tree");
-        attr(object, "index") <- 1:dim(data)[1];
-    } 
-    else {
-        index <- match(rownames(data), phy$tip.label);
-        if (any(is.na(index))) {
-            warning("Rownames in data frame do not match tree tip names; data taken to be in the same order as in tree");
-            attr(object, "index") <- 1:dim(data)[1];
-        } 
-        else {
-            attr(object, "index") <- index;
-        }
-    }
-    return(object);	
-}
-
-corMatrix.corBAMM <- function(object, covariate = getCovariate(object), corr = TRUE, ...)
-{
-    if (!("corBAMM" %in% class(object)))
-        stop("arg 'object' not of class 'corBAMM'");
-    if (!any(attr(object, "index")))
-        stop("arg 'object' has not been initialized");
-    if (is.null(Eb <- attr(object, "Eb")))
-        Eb <- getCohortMatrix(attr(object, "bammdata"));
-    index <- attr(object, "index");
-    return(Eb[index, index]);
-}
-
-coef.corBAMM <- function(object, unconstrained = TRUE, ...)
-{
-    if (!("corBAMM" %in% class(object)))
-        stop('object is not of class "corBAMM"')
-    #return(numeric(0));
-    
-    
-}
-
-
-# This is basically coef
-coef.corBAMM <- function (object, unconstrained = TRUE, ...) 
-{
-    if (unconstrained) {
-        if (attr(object, "fixed")) 
-            return(numeric(0))
-        else return(object[1])
-    }
-    aux <- object[1]
-    names(aux) <- "kappa"
-    aux
-}
-
-
-
-##########
-# Implements the linear mixture error structure:
-# kappa*(corBrownian) + (1-kappa)*corBAMM
-# kappa is estimated during model fitting
+# Implements the linear mixture error structure
+#           psi*lambda*Em + (1-psi)*Eb
+# \params
+#    psi    :: mixture coefficient
+#    lambda :: Pagel's lambda
+# \constants
+#    Em     :: phylogenetic correlation matrix
+#    Eb     :: BAMM correlation matrix
+# 
+# Parameters are supplied via the 'value' argument. If value = numeric(0),
+# the default is to estimate psi (initial value = 0.5) and to treat lambda
+# as a fixed parameter equal to 1.
+#
+# if fixed = TRUE parameters are treated as fixed and not estimated. 
+#
+# Examples:
+#    value = numeric(0),   fixed = FALSE        psi = 0.5(estimated), lambda = 1(fixed)   default
+#    value = c(1,0),       fixed = TRUE         psi = 1(fixed), lambda = 0(fixed)
+#    value = c(0,1),       fixed = TRUE         psi = 0(fixed), lambda = 1(fixed)
+#    value = c(0.5, 0.75)  fixed = FALSE        psi = 0.5(estimated), lambda = 0.75(estimated)
 ##########
 
-corBAMM <- function(value = 0.5, ephy, form = ~1, fixed = FALSE)
+corBAMM <- function(value = numeric(0), ephy, form = ~1, fixed = FALSE)
 {
     if (class(ephy) != "bammdata")
 	    stop("arg 'ephy' is not of class 'bammdata'");
@@ -113,12 +45,32 @@ Initialize.corBAMM <- function(object, data, ...)
     else {
         attr(object, "Dim") <- Dim(object, as.factor(rep(1, nrow(data))));
     }
-
+    val <- as.vector(object);
+    if (length(val) > 0) {
+        if (val[1] < 0 || val[1] > 1)
+            stop("psi must be between 0 and 1");
+        if (val[1] == 0)
+            val[1] <- .Machine$double.eps;
+        val[1] <- log(val[1]) - log(1-val[1]);
+        if (length(val) > 1) {
+            if (val[2] < 0 || val[2] > 1)
+                stop("lambda must be between 0 and 1");
+            if (val[2] == 0)
+                val[2] <- .Machine$double.eps;
+            val[2] <- log(val[2]) - log(1-val[2]);
+        }
+    }
+    else {
+        val <- log(1);
+    }
+    oldAttr <- attributes(object);
+    object <- val;
+    attributes(object) <- oldAttr;
     bammdata <- attr(object, "bammdata");
     phy <- as.phylo.bammdata(bammdata);
     attr(object, "Eb") <- getCohortMatrix(bammdata);
     attr(object, "Em") <- vcv.phylo(phy, corr = TRUE);
-  
+    
     if (nrow(data) != length(phy$tip.label))
         stop("number of observations and number of tips in the tree are not equal");
     if (is.null(rownames(data))) {
@@ -135,7 +87,6 @@ Initialize.corBAMM <- function(object, data, ...)
             attr(object, "index") <- index;
         }
     }
-    
     return (object);   
 }
 
@@ -150,13 +101,35 @@ corMatrix.corBAMM <- function(object, covariate = getCovariate(object), corr = T
     if (is.null(Em <- attr(object, "Em")))
         Em <- vcv.phylo(as.phylo.bammdata(attr(object, "bammdata")), corr = TRUE);
     index <- attr(object, "index");
-    if (object[1] < 1e-7) 
-        object[1] <- 0.0
-    else if (object[1] > 1)
-        object[1] <- 1.0;
-    kappa <- object[1];
-    return(kappa*Em[index,index] + (1-kappa)*Eb[index, index]);
+    psi <- 1/(1+exp(-object[1]));
+    if (length(as.vector(object)) == 1) {
+        Em <- psi*Em[index,index] + (1-psi)*Eb[index, index];
+    }
+    else {
+        lambda <- 1/(1+exp(-object[2]));
+        Em <- psi*lambda*Em[index,index] + (1-psi)*Eb[index, index];
+    }
+    diag(Em) <- 1;
+    return (Em);
 }
+
+# This is basically coef
+coef.corBAMM <- function (object, unconstrained = TRUE, ...) 
+{
+    lx <- function(x) 1/(1+exp(-x));
+    if (unconstrained) {
+        if (attr(object, "fixed")) 
+            return(numeric(0))
+        else return(lx(as.vector(object)));
+    }
+    aux <- as.vector(object);
+    if (length(aux) > 1)
+        names(aux) <- c("psi", "lambda")
+    else
+        names(aux) <- "psi";
+    return (lx(aux));
+}
+
 
 
 
