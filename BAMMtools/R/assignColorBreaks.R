dd1245c74f527ba9fae091963b9441702bec4796
ptitle <pascaltitle@gmail.com>
2015-02-09 17:52:12 -0500

    Add jenks color breaks option to plot.bammdata

--- BAMMtools/R/assignColorBreaks.R
+++ BAMMtools/R/assignColorBreaks.R
@@ -1,17 +1,27 @@
-assignColorBreaks <- function(rates, NCOLORS = 64, spex = "s", logcolor = FALSE, method = c("linear","quantile")) {
-	method = match.arg(method, c("linear", "quantile"));
+assignColorBreaks <- function(rates, NCOLORS = 64, spex = "s", logcolor = FALSE, method = c("linear","quantile","jenks")) {
+	method = match.arg(method, c("linear", "quantile", "jenks"));
 	if (mode(rates) == "numeric") {
 		if (logcolor == FALSE) {
-			if (method == "quantile")
+			if (method == "quantile") {
 				bks <- quantile(rates, seq(0,1, length.out=(NCOLORS+1)))
-			else
+			}
+			if (method == 'jenks') {
+				cat('jenks','\n')
+				bks <- getJenksBreaks(rates, k=(NCOLORS + 1))
+			}
+			if (method == 'linear') {
 				bks <- seq(min(rates), max(rates), length.out = (NCOLORS+1));
+			}
 		}
 		else {
 			if (method == "quantile") {
 				bks <- quantile(log(rates), seq(0,1, length.out=(NCOLORS+1)))
 			}
-			else {
+			if (method == 'jenks') {
+				cat('jenks','\n')
+				bks <- getJenksBreaks(log(rates), k=(NCOLORS + 1))
+			}
+			if (method == 'linear') {
 				bks <- seq(min(log(rates)), max(log(rates)), length.out = (NCOLORS+1));
 			}
 		}	
@@ -19,42 +29,67 @@ assignColorBreaks <- function(rates, NCOLORS = 64, spex = "s", logcolor = FALSE,
 	else if (mode(rates) == "list") {
 		if (tolower(spex) == "s") {
 			if (logcolor == FALSE) {
-				if (method == "quantile")
+				if (method == "quantile") {
 					bks <- quantile(rates[[1]], seq(0,1, length.out=(NCOLORS+1)))
-				else
+				}
+				if (method == 'jenks') {
+					cat('jenks','\n')
+					bks <- getJenksBreaks(rates[[1]], k=(NCOLORS + 1))
+				}
+				if (method == 'linear') {
 					bks <- seq(min(rates[[1]]), max(rates[[1]]), length.out = (NCOLORS+1))
+				}
 			}
 			else {
 				if (method == "quantile") {	
 					bks <- quantile(log(rates[[1]]), seq(0,1, length.out=(NCOLORS+1)))
 				}
-				else {
+				if (method == 'jenks') {
+					cat('jenks','\n')
+					bks <- getJenksBreaks(log(rates[[1]]), k=(NCOLORS + 1))
+				}
+				if (method == 'linear') {
 					bks <- seq(min(log(rates[[1]])), max(log(rates[[1]])), length.out=(NCOLORS+1));
 				}
 			}
 		}
 		else if (tolower(spex) == "e") {
 			if (logcolor == FALSE) {
-				if (method == "quantile")
+				if (method == "quantile") {
 					bks <- quantile(rates[[2]], seq(0,1, length.out=(NCOLORS+1)))
-				else
-					bks <- seq(min(rates[[2]]), max(rates[[2]]), length.out = (NCOLORS+1));
+				}
+				if (method == 'jenks') {
+					cat('jenks','\n')
+					bks <- getJenksBreaks(rates[[2]], k=(NCOLORS + 1))
+				}
+				if (method == 'linear') {
+					bks <- seq(min(rates[[2]]), max(rates[[2]]), length.out = (NCOLORS+1))
+				}
 			}
 			else {
-				if (method == "quantile") {
+				if (method == "quantile") {	
 					bks <- quantile(log(rates[[2]]), seq(0,1, length.out=(NCOLORS+1)))
 				}
-				else {
-					bks <- seq(min(log(rates[[2]])), max(log(rates[[2]])), length.out=(NCOLORS+1));
+				if (method == 'jenks') {
+					cat('jenks','\n')
+					bks <- getJenksBreaks(log(rates[[2]]), k=(NCOLORS + 1))
+				}
+				if (method == 'linear') {
+					bks <- seq(min(log(rates[[2]])), max(log(rates[[2]])), length.out = (NCOLORS+1))
 				}
 			}
 		}
 		else {
 			if (logcolor == FALSE) {
-				if (method == "quantile")	
+				if (method == "quantile") {	
 					bks <- quantile(rates[[1]] - rates[[2]], seq(0,1, length.out=(NCOLORS+1)))
-				else
+				}
+				if (method == 'jenks') {
+					bks <- getJenksBreaks(rates[[1]] - rates[[2]], k=(NCOLORS + 1))
+				}
+				if (method == 'linear') {
 					bks <- seq(min(rates[[1]] - rates[[2]]), max(rates[[1]] - rates[[2]]), length.out=(NCOLORS+1));
+				}
 			}
 			else { 
 				z <- safeLog(rates[[1]] - rates[[2]]);
@@ -62,7 +97,10 @@ assignColorBreaks <- function(rates, NCOLORS = 64, spex = "s", logcolor = FALSE,
 					bks <- quantile(z, seq(0,1, length.out=(NCOLORS+1)))
 					#bks <- quantile(log(rates[[1]] - rates[[2]]), seq(0,1, length.out=(NCOLORS+1)))
 				}
-				else {
+				if (method == 'jenks') {
+					bks <- getJenksBreaks(z, k=(NCOLORS + 1))					
+				}
+				if (method == 'linear') {
 					bks <- seq(min(z), max(z), length.out=(NCOLORS+1) );
 					#bks <- seq(min(log(rates[[1]] - rates[[2]])), max(min(log(rates[[1]] - rates[[2]]))), length.out=(NCOLORS+1) );
 				}
