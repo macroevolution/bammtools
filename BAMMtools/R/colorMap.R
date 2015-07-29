37fd84cb05b0e53ba9741e290dc237b7f53d1e89
ptitle <pascaltitle@gmail.com>
2015-07-29 10:05:23 -0400

    add to colorMap the ability to handle color.intervals that reach outside of the range of rates found in the bammdata object

--- BAMMtools/R/colorMap.R
+++ BAMMtools/R/colorMap.R
@@ -7,7 +7,7 @@
 
 colorMap <- function(x, pal, breaks, logcolor = FALSE, color.interval = NULL) {
     dpal <- get("palettes", envir = .colorEnv);
-	NCOLORS <- length(breaks)-1;
+	NCOLORS <- length(breaks) - 1;
 	if (length(pal) >= 3) {
 		colpalette <- colorRampPalette(pal,space='Lab')(NCOLORS);	
 	} else if (pal %in% names(dpal)) {
@@ -25,34 +25,64 @@ colorMap <- function(x, pal, breaks, logcolor = FALSE, color.interval = NULL) {
 			stop("Color interval must have 2 values.");
 		}
 		if (is.na(color.interval[1])) {
-			color.interval[1] <- min(x);
+			color.interval[1] <- min(breaks);
 		}
 		if (is.na(color.interval[2])) {
-			color.interval[2] <- max(x);
-		}
-		goodbreaks <- intersect(which(breaks > color.interval[1]), which(breaks < color.interval[2]));
-		topcolor <- colpalette[length(colpalette)];
-		bottomcolor <- colpalette[1];
-
-		NCOLORS <- length(goodbreaks) - 1;
-		if (length(pal) >= 3) {
-			colpalette2 <- colorRampPalette(pal,space='Lab')(NCOLORS);	
-		} else if (pal %in% names(dpal)) {
-			colpalette2 <- colorRampPalette(dpal[[pal]],space='Lab')(NCOLORS);
-		} else if (tolower(pal) == "temperature") {
-			colpalette2 <- richColors(NCOLORS);	
-		} else if (tolower(pal) == "terrain") {
-			colpalette2 <- terrain.colors(NCOLORS);
+			color.interval[2] <- max(breaks);
 		}
 		
-		#replace colors in original color ramp
-		colpalette[goodbreaks[1:(length(goodbreaks) - 1)]] <- colpalette2;
-		colpalette[1:(goodbreaks[1]-1)] <- bottomcolor;
-		colpalette[goodbreaks[length(goodbreaks)]:length(colpalette)] <- topcolor;
+		#if color interval is contained within the range of supplied rates
+		if (color.interval[1] >= min(breaks) & color.interval[2] <= max(breaks)) {
+			goodbreaks <- intersect(which(breaks > color.interval[1]), which(breaks < color.interval[2]));
+			topcolor <- colpalette[length(colpalette)];
+			bottomcolor <- colpalette[1];
+			
+			NCOLORS <- length(goodbreaks) - 1;
+			if (length(pal) >= 3) {
+				colpalette2 <- colorRampPalette(pal,space='Lab')(NCOLORS);	
+			} else if (pal %in% names(dpal)) {
+				colpalette2 <- colorRampPalette(dpal[[pal]],space='Lab')(NCOLORS);
+			} else if (tolower(pal) == "temperature") {
+				colpalette2 <- richColors(NCOLORS);	
+			} else if (tolower(pal) == "terrain") {
+				colpalette2 <- terrain.colors(NCOLORS);
+			}
+			
+			#replace colors in original color ramp
+			colpalette[goodbreaks[1:(length(goodbreaks) - 1)]] <- colpalette2;
+			colpalette[1:(goodbreaks[1]-1)] <- bottomcolor;
+			colpalette[goodbreaks[length(goodbreaks)]:length(colpalette)] <- topcolor;
+		}
+
+		#if color interval exceeds the range of color breaks
+		if (color.interval[1] < min(breaks) | color.interval[2] > max(breaks)) {
+			
+			#generate new set of breaks for full range of rate values
+			newbreaks <- seq(from = min(color.interval[1], min(breaks)), to = max(color.interval[2], max(breaks)), by = (breaks[2] - breaks[1]));
+			newbreaks <- seq(from = min(color.interval[1], min(breaks)), to = max(color.interval[2], max(breaks)), length.out = length(newbreaks));
+			
+			goodbreaks <- intersect(which(newbreaks >= color.interval[1]), which(newbreaks <= color.interval[2]));
+			
+			#generate colors for new breaks
+			NCOLORS <- length(goodbreaks) - 1;
+			if (length(pal) >= 3) {
+				colpalette2 <- colorRampPalette(pal,space='Lab')(NCOLORS);	
+			} else if (pal %in% names(dpal)) {
+				colpalette2 <- colorRampPalette(dpal[[pal]],space='Lab')(NCOLORS);
+			} else if (tolower(pal) == "temperature") {
+				colpalette2 <- richColors(NCOLORS);	
+			} else if (tolower(pal) == "terrain") {
+				colpalette2 <- terrain.colors(NCOLORS);
+			}
+			
+			#identify max and min colors that define the rates in the bammdata object
+			breaksStart <- which(sapply(1:length(newbreaks), function(x) min(breaks) >= newbreaks[x] & min(breaks) <= newbreaks[x + 1]));
+			breaksEnd <- which(sapply(1:length(newbreaks), function(x) max(breaks) >= newbreaks[x] & max(breaks) <= newbreaks[x + 1]));
+			
+			colpalette <- colpalette2[breaksStart : breaksEnd];
+		}
 	}
 	
-#	if (logcolor) x <- log(x);
-
 	kde <- density(x, from=min(x), to=max(x));
 	colset <- numeric(length(x));
 	coldens <- numeric(length(kde$x));
